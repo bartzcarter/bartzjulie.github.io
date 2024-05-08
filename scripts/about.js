d1de0a81b0f33f2e122f80b0dbc5cdecf1acf5a9
Carter Bartz <carterbartz@Carters-MacBook-Pro.local>
2024-05-08 11:44:45 -0600

    connect google sheets to contact form

--- scripts/about.js
+++ scripts/about.js
@@ -34,3 +34,20 @@ function opentab(event, tabname) {
     // Add "active-tab" class to the corresponding tab content
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz5cwYQ3UPRK3h7grfAG34EV863yLh3CJn0dENaSnuxKw9aU2H9XtnEIHXvyjaJ_w/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
